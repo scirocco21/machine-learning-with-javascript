bb3aa629c02fd22b38bc5e499bdd909c01c7d3f0
Sebastian <sebgertz@gmail.com>
2019-01-30 22:27:54 -0500

    add basic knn implementation

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,27 @@
+const outcomes = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+   outcomes.push([dropPosition, bounciness, size, bucketLabel])
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-}
+  const predictionPoint = 300;
+  const k = 5;
 
+  function distance(point) {
+    return Math.abs(point - predictionPoint);
+  }
+
+  const predictedBucket = _.chain(outcomes)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0,k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+}
